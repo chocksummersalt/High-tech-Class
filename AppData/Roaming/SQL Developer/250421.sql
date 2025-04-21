fdf86251c83ef3cb65ee2b897cb7a2a964b51329
chocksummersalt <>
2025-04-21 15:28:55 +0900

    sql select  from,  || pract

---
+++ AppData/Roaming/SQL Developer/250421.sql
@@ -0,0 +1,11 @@
+SELECT ename ||'''s sal is $' || Sal "NAME and SAL" 
+FROM emp ;
+
+SET pagesize 50
+
+SELECT name ||' ''s ID :' || ID ||' WEIGHT is '|| weight
+
+FROM student  
+
+SELECT ENAME||'('||job||') , '||ENAME|| ''''||job||'''' "NAME AND JOB" 
+FROM emp
